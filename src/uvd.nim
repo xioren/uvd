bf8a4b5c034e315147469cdef6d1b2495b092dec
xioren <xioren00+github@protonmail.com>
2021-09-11 16:11:07 -0700

    update readme

--- src/uvd.nim
+++ src/uvd.nim
@@ -13,8 +13,8 @@ proc main() =
       -v, --video                     Video Only
       -f, --format                    Audio Output Format
       -s, --show                      Show Available Streams
-      -ai, -aid, --aitag              Audio Stream id/itag
-      -vi, -vid, --vitag              Video Stream id/itag
+      -ai, --aid, --aitag              Audio Stream id/itag
+      -vi, --vid, --vitag              Video Stream id/itag
       -h, --help                      Print This Help
     """
   # WIP
